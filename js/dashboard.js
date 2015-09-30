fc8cf500f8655eedac1047e4a826949a7a0f9710
Chetan Kantharia <chetan.kantharia@raweng.com>
2015-09-30 12:09:12 +0530

    kingsapp: - logout button added in ui

--- js/dashboard.js
+++ js/dashboard.js
@@ -4,39 +4,7 @@ angular.module('dashboard', ['kings-app.utils'])
   '$q',
   '$location',
   function($scope, $q, $location){
-
-    query.toJSON().exec()
-        .then(function(objects){
-            $scope.newLists = objects;
-            deferred.resolve(objects);
-            return deferred.promise;
-        },function(err){
-            deferred.reject(err);
-            return deferred.promise;
-        });
-
-    $scope.newPlayer = function(){
-        $sa($scope, function(){
-            $location.path('/new');
-        })
+    $scope.signOut = function(){
+      console.log('Hey!');
     }
-
-    $scope.delete = function(){
-        $scope.showModal = true;
-    }
-    $scope.close = function(){
-        $scope.showModal = false;
-    }
-    $scope.ok = function(player){
-        /*var del = BuiltClass.Object(player);
-        del.delete().then(function(){
-            console.log('object deleted successfully');
-        })*/
-        $scope.showModal = false;
-    }
-
-    $scope.edit = function(){
-
-    }
-
 }]);
