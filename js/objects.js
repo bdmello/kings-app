120fc491cc11c08bbb27530e4312faedef35a418
Chetan Kantharia <chetan.kantharia@raweng.com>
2015-10-01 15:47:07 +0530

    noop

--- js/objects.js
+++ js/objects.js
@@ -3,7 +3,10 @@ angular.module('objects',[])
 		'$scope',
 		'currentClass',
 		'user',
-		function($scope, currentClass, user){
+		'builtApi',
+		function($scope, currentClass, user, builtApi){
 			console.log('mclass',currentClass.data.class);
 			console.log('authtoken', user.data.user.authtoken);
+			console.log('api_key', builtApi.getAppConfig().api_key);
+			console.log('api_host', builtApi.getAppConfig().apihost);
 		}]);
