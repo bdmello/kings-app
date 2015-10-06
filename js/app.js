de538a4a4ebf328d8d3cf47eed2496351abb75af
bruno <bruno.dmello@raweng.com>
2015-10-06 20:15:13 +0530

    noop

--- js/app.js
+++ js/app.js
@@ -85,10 +85,14 @@ kingsapp.config([
    
     
     dataServiceProvider.setAppConfig({
-        // apihost:"https://kings-backend.built.io/v1",
-        // url : "https://kings-backend.built.io",
-        apihost:'http://code-bltdev.cloudthis.com/v1',
-        url:window.location.protocol+'//'+ window.location.host,
+        /*Prod*/
+        apihost:"https://kings-backend.built.io/v1",
+        url : "https://kings-backend.built.io",
+        /*Dev*/
+        // url : "http://code-bltdev.cloudthis.com",
+        // apihost:'http://code-bltdev.cloudthis.com/v1',
+
+        //url:window.location.protocol+'//'+ window.location.host,
         version:"/v1",
         api_key : "bltbfb51fc159335dd8"
     })
