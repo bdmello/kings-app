de2648fe80cd058ba28947f0fc8ae71b9180d771
Chetan Kantharia <chetan.kantharia@raweng.com>
2015-10-06 14:28:50 +0530

    noop

--- js/controllers/dashboard/dashboard.js
+++ js/controllers/dashboard/dashboard.js
@@ -11,11 +11,7 @@ angular.module('kings-app.dashboard', ['kings-app.providers'])
   'user',
   function($scope, $q, $location, $http, dataService, menus, $state, user){
     $scope.$emit('user', user);
-    console.log("$state", $state);
     $scope.menus = menus;
-    console.log('---Menus', menus);
-
     //set class id
     $scope.selectedClassId = $state.params.classUid;
-
 }]);
